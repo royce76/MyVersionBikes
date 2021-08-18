9ff52d5b341911b462d92889d5fb88cb32408328
Royce George <royce.george.pro@outlook.fr>
2021-08-18 15:41:30 +0200

    first update inline edit

--- force-app/main/default/lwc/myBikesTable/myBikesTable.js
+++ force-app/main/default/lwc/myBikesTable/myBikesTable.js
@@ -1,15 +1,18 @@
-import { api, LightningElement, wire } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import PF from '@salesforce/messageChannel/ProductsFiltered__c';
+
 import {
   subscribe,
+  publish,
   APPLICATION_SCOPE,
   MessageContext
 } from 'lightning/messageService';
+
 import { refreshApex } from '@salesforce/apex';
 import { updateRecord } from 'lightning/uiRecordApi';
 
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import NAME_FIELD from '@salesforce/schema/Product__c.FirstName';
+import NAME_FIELD from '@salesforce/schema/Product__c.Name';
 import ID_FIELD from '@salesforce/schema/Product__c.Id';
 
 const columnField = [
@@ -25,17 +28,13 @@ export default class MyBikesTable extends LightningElement {
   @wire(MessageContext)
   messageContext;
 
-  @api
-  recordId;
+  //When a user edits a cell, the updated value is stored in draft-values
+  draftValues = [];
 
-  /**
-   * Data for my datatable
-   */
+  //Data for my datatable
   bikes;
 
-  /**
-   * Property for my datatable
-   */
+  //Property for my datatable
   columns = columnField;
 
   subscription = null;
@@ -54,13 +53,48 @@ export default class MyBikesTable extends LightningElement {
   // Handler for message received by component
   handleMessage(message) {
     this.bikes = message.filters;
-    this.bikes = this.bikes.map((bike)=>{
+    /* this.bikes = this.bikes.map((bike)=> {
       let mountain = bike.Category__c === 'Mountain' ? 'utility:animal_and_nature': '';
       return {...bike, 'mountainBike': mountain}
-    });
+    }); */
   }
 
   connectedCallback() {
     this.subscribeToMessageChannel();
   }
+
+  handleSave(event) {
+    const fields = {};
+    fields[ID_FIELD.fieldApiName] = event.detail.draftValues[0].Id;
+    fields[NAME_FIELD.fieldApiName] = event.detail.draftValues[0].Name;
+
+    const recordInput = { fields };
+
+    updateRecord(recordInput)
+    .then( (result) => {
+      console.log(result);
+      console.log(this.bikes);
+      for (const item of this.bikes) {
+        console.log(item);
+      }
+    })
+    .then( () => {
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: 'Success',
+          message: 'Contact updated',
+          variant: 'success'
+        })
+      );
+    }).catch( () => {
+      this.dispatchEvent(
+        new ShowToastEvent({
+            title: 'Error updating or reloading record',
+            message: 'oups',
+            variant: 'error'
+        })
+      );
+    })
+  }
+
 }
